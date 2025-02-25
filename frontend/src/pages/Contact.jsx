f190184b5e9bc5def7edf60c6db3455e1df7c2c9
akashreddy1234 <akashreddybiyyam@gmail.com>
2025-02-25 14:25:15 +0530

    initial commit

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className=' text-gray-500'> AARA Fashion point fancy dresses,<br /> watertank, Sec a rajpura,Andhrapradesh 515001</p>
-          <p className=' text-gray-500'>Tel: 7780370596 <br /> Email: akashreddybiyyam@gmail.com</p>
+          <p className=' text-gray-500'>Tel: 9014781656 <br /> Email: akashreddybiyyam@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Fashionpoint</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
