14176116f582a6fecbc7396fcfc27a6d45ad9e6e
Akash Reddy <akashreddybiyyam@gmail.com>
2025-07-24 10:07:17 +0530

    heiuy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
@@ -8,13 +11,13 @@ import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
 
-// App Config
 const app = express()
 const port = process.env.PORT || 4000
+
 connectDB()
 connectCloudinary()
 
-// middlewares
+// middleware
 app.use(express.json())
 
 app.use(cors({
@@ -23,19 +26,22 @@ app.use(cors({
     'https://fashion-website-sdj8.vercel.app'  // Admin Panel
   ],
   credentials: true,
-}));
+}))
 
+// API Routes
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
+// Serve React frontend
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+app.use(express.static(path.join(__dirname, 'dist')))
 
-// api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-
-app.get('/',(req,res)=>{
-    res.send("API Working")
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
-app.listen(port, ()=> console.log('Server started on PORT : '+ port))
+app.listen(port, () => console.log('Server started on PORT : ' + port))
